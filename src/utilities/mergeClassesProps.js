033e4a91bfbb96bc843e7b167fd8674a4cd2f94d
Jason K. Frank <jasonkylefrank@gmail.com>
2019-02-21 17:26:19 -0500

    Added example inputs and output to the merge utility.

--- src/utilities/mergeClassesProps.js
+++ src/utilities/mergeClassesProps.js
@@ -1,14 +1,37 @@
 import classnames from 'classnames';
 
-// This function provides a convenient way to merge 2 objects containing CSS
-//  class names which are meant to be supplied to Material UI controls via
-//  their "classes" prop.  For each key which is represented in both objects,
-//  the corresponding values of those shared keys will be merged such that the
-//  new values contain BOTH values.  In this way, this merge function creates 
-//  and returns a new object that applies the CSS class names of both sets of 
-//  input objects.
-// The typical use-case is where we are trying to apply both "internal" classes
-//  and "external" classes (ones passed-in by consuming components). 
+/*  This function provides a convenient way to merge 2 objects containing CSS
+    class names which are meant to be supplied to Material UI controls via
+    their "classes" prop.  For each key which is represented in both objects,
+    the corresponding values of those shared keys will be merged such that the
+    new values contain BOTH values.  In this way, this merge function creates 
+    and returns a new object that applies the CSS class names of both sets of 
+    input objects.
+
+    The typical use-case is where we are trying to apply both "internal" classes
+    and "external" classes (ones passed-in by consuming components). 
+
+    Example inputs:
+
+    const internalClasses = {
+        root: '.rootClassName',
+        label: '.labelClassName',
+        contained: '.containedClassName',
+    }
+    const externalClasses = {
+        root: '.someClassName',
+        somethingElse: '.anotherName'
+    };
+
+    Example output:
+
+    {
+        root: '.rootClassName .someClassName',
+        label: '.labelClassName',
+        contained: '.containedClassName',
+        somethingElse: '.anotherName'
+    }
+*/
 const mergeClassesProps = (internalClasses, externalClasses) => {
     if (!externalClasses) {
         return internalClasses;
